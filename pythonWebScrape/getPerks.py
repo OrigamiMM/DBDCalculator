afe2b0c342e60824ee9d7e7fee8083226abafc65
Oscar <oacevedo@ucsd.edu>
2023-08-07 23:16:20 -0700

    starting to make requests

--- pythonWebScrape/getPerks.py
+++ pythonWebScrape/getPerks.py
@@ -1,1 +1,31 @@
-print('hi')
+import json
+import requests
+from bs4 import BeautifulSoup
+url = 'https://deadbydaylight.fandom.com/wiki/Survivor_Perks'
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+data = {
+    'perks' : []
+}
+
+entries = soup.find_all('tr')
+counter = 0
+for entry in entries[1:]:
+    # data['perks'].append({
+    #     'name': entry.find_all('a')[1].text
+    # })
+    col = entry.children
+    print(type(col))
+
+    break
+
+# print(tbody.prettify())
+
+
+# json_string = json.dumps(data, indent=2)
+# with open('perks.json', 'w') as outfile:
+#     outfile.write(json_string)
+
