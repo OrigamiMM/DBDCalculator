b5370c1b626f172d5a27327673e1cd09f4bf7f6f
Oscar <oacevedo@ucsd.edu>
2023-08-07 19:22:16 -0700

    created first JSON object and write to file

--- pythonWebScrape/getPerks.py
+++ pythonWebScrape/getPerks.py
@@ -1,1 +1,17 @@
-print('hi')
+import json
+url = 'https://deadbydaylight.fandom.com/wiki/Survivor_Perks'
+
+data = {
+    'perks' : []
+}
+
+perk1 = {'name': "We'll Make it"}
+perk2 = {'name': "Ace in the Hole"}
+
+data['perks'].append(perk1)
+data['perks'].append(perk2)
+
+json_string = json.dumps(data, indent=2)
+print(json_string)
+with open('perks.json', 'w') as outfile:
+    outfile.write(json_string)
