a861d09ceba1accc3eee52aaed8f81912845d697
Oscar <oacevedo@ucsd.edu>
2023-08-08 18:04:56 -0700

    added getPerks functionality

--- pythonWebScrape/getPerks.py
+++ pythonWebScrape/getPerks.py
@@ -14,18 +14,29 @@ data = {
 entries = soup.find_all('tr')
 counter = 0
 for entry in entries[1:]:
-    # data['perks'].append({
-    #     'name': entry.find_all('a')[1].text
-    # })
-    col = entry.children
-    print(type(col))
+    trs = entry.find_all("th")
+    tdText = entry.find("td")
+    
+    #wired encoding for accented characters
+    name = trs[1].find('a')
+    description = tdText.find("div", class_="formattedPerkDesc").decode_contents()
+    imgUrl = trs[0].find('a')['href']
+    source = 'Any'
+    sourceUrl = None
+    
+    if(len(trs[2].find_all('a')) == 2):
+        source = trs[2].find_all('a')[0]['href'][6:].replace('_', ' ')
+        sourceUrl = trs[2].find_all('a')[1]['href']
 
-    break
+    data['perks'].append({
+        'name':  name,
+        'description':  description,
+        'imgUrl': imgUrl,
+        'source': source,
+        'sourceUrl': sourceUrl
+    })
 
-# print(tbody.prettify())
-
-
-# json_string = json.dumps(data, indent=2)
-# with open('perks.json', 'w') as outfile:
-#     outfile.write(json_string)
+json_string = json.dumps(data, indent=2)
+with open('perks.json', 'w') as outfile:
+    outfile.write(json_string)
 
