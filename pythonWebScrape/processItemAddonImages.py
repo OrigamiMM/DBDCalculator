60ab4c90988443c49b146d2d4f07a1386cb3dfeb
Oscar <oacevedo@ucsd.edu>
2023-09-19 23:15:18 -0700

    updated methods for file names to fit with cloudify ids

--- pythonWebScrape/processItemAddonImages.py
+++ pythonWebScrape/processItemAddonImages.py
@@ -1,31 +1,36 @@
 import json
 import urllib.request
 from PIL import Image
+import re
   
 with open('itemsAndAddons.json') as f:
     data = json.load(f)
 
+pattern = "[^0-9a-zA-Z]+"
 items = data['items']
+
 
 for itemType in items:
     for item in items[itemType]:
-        imgName = '{}.png'.format(item['name'].replace(" ", "").replace(':',""))
+        #imgName = '{}.png'.format(item['name'].replace(" ", "").replace(':',""))
+        imgName = '{}.png'.format(re.sub(pattern, '',item['name']))
         savedPath = imgName
-        path = '../assets/itemImages/{}'.format(imgName)
-        urllib.request.urlretrieve(item['imgUrl'], imgName)
-        img = Image.open(imgName)
-        img.save(path, 'png')
+        # path = '../assets/itemImages/{}'.format(imgName)
+        #urllib.request.urlretrieve(item['imgUrl'], imgName)
+        # img = Image.open(imgName)
+        # img.save(path, 'png')
         item['imgUrl'] = savedPath
 
 addons = data['addons']
 for addonType in addons:
     for addon in addons[addonType]:
-        imgName = '{}.png'.format(addon['name'].replace(" ", "").replace(':',""))
+        #imgName = '{}.png'.format(addon['name'].replace(" ", "").replace(':',""))
+        imgName = '{}.png'.format(re.sub(pattern, '',addon['name']))
         savedPath = imgName
-        path = '../assets/addonImages/{}'.format(imgName)
+        # path = '../assets/addonImages/{}'.format(imgName)
         urllib.request.urlretrieve(addon['imgUrl'], imgName)
-        img = Image.open(imgName)
-        img.save(path, 'png')
+        # img = Image.open(imgName)
+        # img.save(path, 'png')
         addon['imgUrl'] = savedPath
 
 json_string = json.dumps(data, indent=2)
--- pythonWebScrape/processPerkImages.py
+++ pythonWebScrape/processPerkImages.py
@@ -1,16 +1,20 @@
 import json
 import urllib.request
 from PIL import Image
+import re
   
 with open('perks.json') as f:
     data = json.load(f)
 
 perks = data['perks']
+pattern = "[^0-9a-zA-Z]+"
 
 for perk in perks:
-    imgName = '{}.png'.format(perk['name'].replace(" ", "").replace(':',""))
+    #imgName = '{}.png'.format(perk['name'].replace(" ", "").replace(':',""))
+    imgName = '{}.png'.format(re.sub(pattern, '', perk['name']))
+
     path = '../assets/perkImages/{}'.format(imgName)
-    #urllib.request.urlretrieve(perk['imgUrl'], imgName)
+    urllib.request.urlretrieve(perk['imgUrl'], imgName)
     #img = Image.open(imgName)
     #img.save(path, 'png')
     perk['imgUrl'] = imgName
